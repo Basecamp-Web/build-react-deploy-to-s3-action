ab04080bc5749f911a2a28d043134b1b39ca857d
dabruge <51396276+dabruge@users.noreply.github.com>
2025-06-26 10:17:29 +0100

    install events

--- entrypoint.sh
+++ entrypoint.sh
@@ -47,6 +47,7 @@ sh -c "npm install" \
 && sh -c "npm install vue-template-compiler" \
 && sh -c "npm install tailwindcss" \
 && sh -c "npm install @tailwindcss/cli" \
+&& sh -c "npm install events" \
 && sh -c "npm run build" \
 && sh -c "aws s3 sync ${SOURCE_DIR:-public} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
               --profile react-deploy-to-s3-action \
